17c6491c4023041ffaf8d0799c4571b0895bdfb0
Shahvir <shahvirb@gmail.com>
2018-11-18 17:52:33 -0600

    cleanup venvbuild.py

--- venvbuild.py
+++ venvbuild.py
@@ -3,23 +3,18 @@ import os
 import logging
 import subprocess
 
-logging.basicConfig(level=logging.DEBUG)
 
 def create_venv(path):
-    #return venv.create(path, with_pip=True)
     virtualenv.create_environment(path)
-    # with open(os.path.join(path, 'scripts', 'activate_this.py')) as source_file:
-    #     exec(source_file.read())
-    #
-    # import site;
-    # logging.debug(site.getsitepackages())
 
 
 def pip_install_requirements(venv_path, requirements):
     cmd = [os.path.join(venv_path, 'Scripts', 'pip.exe'), 'install', '-r', requirements]
     subprocess.run(cmd)
 
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     PATH = os.path.join(os.getcwd(), 'repo_venv')
     create_venv(PATH)
     pip_install_requirements(PATH, os.path.join(os.getcwd(), 'requirements.txt')) #TODO
