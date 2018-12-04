bc6a949a1e77a6e8be6ebcde6a9edc9576645044
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-03 22:16:47 -0600

    made auto_size_y

--- statplot.py
+++ statplot.py
@@ -4,25 +4,22 @@ def stat_plot(df, res_path='stats.png'):
     fig, ax = plt.subplots(figsize=(19.2, 10.8))
     ax.plot(df, '.')
     ax.grid(True)
-    ax.set_ylabel(df.columns[0])
 
-    max_val = df.max()[0]
-    if  max_val > 1000:
-        x_max = 5000
-    elif max_val > 500:
-        x_max = 1000
-    elif max_val > 250:
-        x_max = 500
-    elif max_val > 100:
-        x_max = 250
-    elif max_val > 50:
-        x_max = 100
-    ax.set_ylim(0, x_max)
+    ax.set_ylabel(df.columns[0])
+    auto_size_y(ax, df)
 
     ax.set_xlim(0, df.index[-1])
 
     fig.savefig(res_path)
     plt.close(fig)
+
+def auto_size_y(ax, df):
+    sizes = [5000, 1000, 500, 250, 100, 50, 10]
+    max_val = df.max()[0]
+    for i, s in enumerate(sizes[1:]):
+        if max_val > s:
+            ax.set_ylim(0, sizes[i])
+            return
 
 if __name__ == '__main__':
     from stats import collect_dataframe
@@ -32,6 +29,6 @@ if __name__ == '__main__':
     start, df = collect_dataframe(
         REGISTRATION[-1],
         Path(r'C:\Users\lanca_000\Documents\Software\Python\AoC Benchmark\AoC-Inputs'),
-        100
+        20
     )
     stat_plot(df)
