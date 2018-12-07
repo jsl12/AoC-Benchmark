bbf5ecd5873ae9ebc52d319a2da789f8e5c06eb2
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-07 12:42:58 -0600

    reformatted result data

--- prof.py
+++ prof.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import cProfile
 import pstats
 import sys
-import os
 import pickle
 
 import click
@@ -67,20 +66,17 @@ def profile_repo(repo_path, input_path, n, users_config, username, timeout):
 
     cstats = str(cfg.working_dir(users_config)  / 'cstats.temp')
 
-    res = {}
+    res = []
     for id, DUT in REGISTRATION:
         # print('Starting profile of {}'.format(day))
         print(' {} '.format(id).center(50, '='))
-
-        res[id] = {}
-
         print('Registered solution function: {}.{}'.format(DUT.__module__, DUT.__name__))
 
         input = get_input(id, input_path)
 
         print('Assessing memory usage...')
-        res[id]['Memory'] = memory_usage((DUT, (), {'input': input}), max_usage=True)[0]
-        print('{:.2f} MB'.format(res[id]['Memory']))
+        mem_use = memory_usage((DUT, (), {'input': input}), max_usage=True)[0]
+        print('{:.2f} MB'.format(mem_use))
 
         print('Initial solution run:')
         cProfile.runctx('DUT(input)', globals=globals(), locals=locals(), filename=cstats)
@@ -93,18 +89,22 @@ def profile_repo(repo_path, input_path, n, users_config, username, timeout):
             n = n_timeout
             print('Adjusting to {} runs'.format(n))
 
-        print('Starting {} runs...'.format(n))
-        res[id]['Time'] = np.empty(n)
-        total_time = 0
-        for i in range(n):
-            cProfile.runctx('DUT(input)', globals=globals(), locals=locals(), filename=cstats)
-            stats = pstats.Stats(cstats)
-            t = extract_time(stats, DUT)
-            total_time += t
-            res[id]['Time'][i] = t
+        times = np.empty(n)
         if n > 0:
+            print('Starting {} runs...'.format(n))
+            total_time = 0
+            for i in range(n):
+                cProfile.runctx('DUT(input)', globals=globals(), locals=locals(), filename=cstats)
+                stats = pstats.Stats(cstats)
+                t = extract_time(stats, DUT)
+                total_time += t
+                times[i] = t
             avg_time = total_time / n
             print('{:.1f} ms average'.format(avg_time))
+        else:
+            print('Not enough time for more runs')
+
+        res.append((id, {'Memory': mem_use, 'Time': times}))
 
     with open(cfg.result_dir(username, users_config), 'wb') as file:
         pickle.dump(res, file)
