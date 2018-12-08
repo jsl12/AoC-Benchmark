2739cdea55f98894a3780b990b9c5506b1c82a6b
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-08 16:08:00 -0600

    removed properties because the ability to set the input_dir and working_dir after loading is unnecessary at this point

--- config.py
+++ config.py
@@ -9,48 +9,15 @@ class Config:
         if isinstance(path, str):
             path = Path(path)
         assert isinstance(path, Path), 'path must be able to be turned into a Path object'
-        self._path = path.resolve()
+        self.path = path.resolve()
         self.yaml = yaml.load(open(path, 'r'))
 
+        self.working_dir = Path(self.yaml['working_dir']).resolve()
         self.res_filename = self.yaml.get('res_filename', 'res.pickle')
         self.timeout = self.yaml.get('timeout', -1)
-        self.working_dir = self.yaml['working_dir']
         self.inputs_url = self.yaml['input']['repo_url']
         self.inputs_dir = self.working_dir / self.yaml['input']['repo_local']
-
         self.users = self.yaml['users']
-
-    def abs_path(self, path):
-        if isinstance(path, str):
-            path = Path(path)
-        if path.is_absolute():
-            return path
-        else:
-            return path.resolve()
-
-    @property
-    def path(self):
-        return self._path
-
-    @path.setter
-    def path(self, path):
-        self.load(path)
-
-    @property
-    def working_dir(self):
-        return self._working_dir
-
-    @working_dir.setter
-    def working_dir(self, path):
-        self._working_dir = self.abs_path(path)
-
-    @property
-    def inputs_dir(self):
-        return self._inputs_dir
-
-    @inputs_dir.setter
-    def inputs_dir(self, path):
-        self._inputs_dir = self.abs_path(path)
 
     def repo_url(self, username):
         return self.users[username]['repo_url']
