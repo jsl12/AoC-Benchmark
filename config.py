14a5199db7788e9f95ac2065df9291031cce1449
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-08 19:54:37 -0600

    better handling of absolute paths

--- config.py
+++ config.py
@@ -5,14 +5,16 @@ class Config:
     def __init__(self, path):
         self.load(path)
 
-    def load(self, path):
-        if isinstance(path, str):
-            path = Path(path)
-        assert isinstance(path, Path), 'path must be able to be turned into a Path object'
-        self.path = path.resolve()
-        self.yaml = yaml.load(open(path, 'r'))
+    def load(self, yaml_path):
+        if isinstance(yaml_path, str):
+            yaml_path = Path(yaml_path)
+        assert isinstance(yaml_path, Path), 'path must be able to be turned into a Path object'
+        self.path = yaml_path.resolve()
+        self.yaml = yaml.load(open(yaml_path, 'r'))
 
-        self.working_dir = Path(self.yaml['working_dir']).resolve()
+        self.working_dir = Path(self.yaml['working_dir'])
+        if not self.working_dir.is_absolute():
+            self.working_dir = self.path.parents[0] / self.working_dir
         self.res_filename = self.yaml.get('res_filename', 'res.pickle')
         self.timeout = self.yaml.get('timeout', -1)
         self.inputs_url = self.yaml['input']['repo_url']
