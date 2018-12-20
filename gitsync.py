0783b3ace73b9689b285d87033967b6db3b87d7b
jsl12 <32917998+jsl12@users.noreply.github.com>
2018-12-20 00:07:34 -0600

    WIP for handling syncing things with git better, about to refactor some

--- gitsync.py
+++ gitsync.py
@@ -1,24 +1,48 @@
 import git
-import os
+from pathlib import Path
 import logging
 
 
-def sync_repo(repo_url, dest_dir):
-    try:
-        logging.info('Cloning {} to {}'.format(repo_url, dest_dir))
-        repo = git.Repo.clone_from(repo_url, dest_dir, branch='master')
-    except git.exc.GitCommandError as e:
-        if e.status == 128:
-            logging.info('   Syncing {} to {}'.format(repo_url, dest_dir))
-            g = git.cmd.Git(dest_dir)
-            g.pull()
-            #TODO get this working
-            return None
+def sync_repo(repo_url, dest_dir, branch='master', remote='origin'):
+    # convert to Path object if it isn't already
+    if not isinstance(dest_dir, Path):
+        dest_dir = Path(dest_dir)
+
+    # check to see if directory already exists. If it doesn't, then we can clone into it no problem
+    if not dest_dir.exists():
+        logging.info('Directory not found: {}'.format(dest_dir))
+        logging.info('Cloning from {}'.format(repo_url))
+        repo = git.Repo.clone_from(repo_url, dest_dir, branch=branch)
+    else:
+        try:
+            logging.info('Attempting to create repo from {}'.format(dest_dir))
+            repo = git.Repo(dest_dir)
+        except git.InvalidGitRepositoryError as e:
+            logging.info('Could not make repo from {}'.format(dest_dir))
+            repo = git.Repo.init(dest_dir)
+
+        if remote not in repo.remotes:
+            repo.create_remote(remote, repo_url)
+
+    # Should have a repo setup by now, no matter what
+    info = repo.remotes[remote].pull()
+
+    # try:
+    #
+    #     repo = git.Repo.clone_from(repo_url, dest_dir, branch='master')
+    # except git.exc.GitCommandError as e:
+    #     if e.status == 128:
+    #         logging.info('   Syncing {} to {}'.format(repo_url, dest_dir))
+    #         g = git.cmd.Git(dest_dir)
+    #         g.pull()
+    #         #TODO get this working
+    #         return None
     return repo.working_dir
 
 
 if __name__ == "__main__":
+    import config
     logging.basicConfig(level=logging.DEBUG)
-    REPO_URL = 'https://github.com/shahvirb/adventOfCode2017'
-    PATH = os.path.join(os.getcwd(), 'repo')
+    REPO_URL = 'https://github.com/jsl12/AoC-Solutions'
+    PATH = config.Config(Path('users.yaml')).working_dir / 'test_sync'
     sync_repo(REPO_URL, PATH)
